30da2e643b8bdd829e1dfd404bb34ffb250959b9
Om Savani <156919604+om-savani@users.noreply.github.com>
2024-01-27 20:02:04 +0530

    1.c

--- PR-1/1.c
+++ PR-1/1.c
@@ -1,12 +1,16 @@
 #include<stdio.h>
+#include<conio.h>
 
 main()
 {
 	float F,C;
+	clrscr():
 	printf("Enter temperature in celsius : ");
 	scanf("%f",&C);
 	
 	F = (C*1.8)+32;
 	
 	printf("Temperature in Fahrenheit : %.2f",F);
+
+	getch();
 }
