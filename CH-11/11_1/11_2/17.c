a0f6684d6ff2aae45bbce7b121188a2ff5084546
Om Savani <156919604+om-savani@users.noreply.github.com>
2024-02-14 09:30:44 +0530

    C-Language

---
+++ CH-11/11_1/11_2/17.c
@@ -0,0 +1,24 @@
+#include<stdio.h>
+#include<conio.h>
+
+main()
+{
+	char i,j,s;
+	//clrscr();
+	
+	for(i='A';i<='E';i++)
+	{
+		for(s='A';s<i;s++)
+		{
+			printf("  ");
+		}
+		
+		for(j=i;j<='E';j++)
+		{
+			printf("%c ",j);
+		}
+		printf("\n");
+	}
+	
+	getch();
+}
