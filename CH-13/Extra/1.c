a6d343afa484272bc47964937bc3af61ca7ed1f8
Om Savani <156919604+om-savani@users.noreply.github.com>
2024-02-29 06:50:27 -0800

    C-Language

---
+++ CH-13/Extra/1.c
@@ -0,0 +1,35 @@
+#include<stdio.h>
+
+main()
+{
+	int i,j,m,s;
+	
+	printf("Enter Size of Array : ");
+	scanf("%d",&m);
+	int a[m];
+	
+	for(i=0;i<m;i++)
+	{
+		printf("Enter a[%d] : ",i);
+		scanf("%d",&a[i]);
+	}
+	
+	for(i=0;i<m;i++)
+	{
+		for(j=i+1;j<m;j++)
+		{
+			if(a[i]>a[j])
+			{
+				s = a[i];
+				a[i] = a[j];
+				a[j] = s;
+			}
+		}
+	}
+	
+	for(i=0;i<m;i++)
+	{
+		printf("\na[%d] : %d\n",i,a[i]);
+	}
+	
+}
